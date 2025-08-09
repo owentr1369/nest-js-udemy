6c828b8b0926e79ff2ed016e8f6fc0fa01e954df
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-09 22:40:42 +0700

    reading and writing to a storage file

--- messages/src/messages/messages.repository.ts
+++ messages/src/messages/messages.repository.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string): Promise<string | undefined> {
@@ -7,7 +7,26 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll(): Promise<string[]> {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents) as Record<string, string>;
+    return Object.values(messages);
+  }
 
-  async create() {}
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents) as Record<
+      string,
+      {
+        content: string;
+        id: number;
+      }
+    >;
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = {
+      content,
+      id,
+    };
+    await writeFile('messages.json', JSON.stringify(messages, null, 2), 'utf8');
+  }
 }
