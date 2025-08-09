ed0c94339b47bdb62c193a37ce59bb7802e32a3f
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-09 22:44:35 +0700

    implementing a service

---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  messageRepo: MessagesRepository;
+  constructor() {
+    // Service is creating its own dependencies
+    // DON'T DO THIS ON REAL APPS
+    this.messageRepo = new MessagesRepository();
+  }
+
+  async findOne(id: string) {
+    return this.messageRepo.findOne(id);
+  }
+
+  async findAll() {
+    return this.messageRepo.findAll();
+  }
+
+  async create(content: string) {
+    return this.messageRepo.create(content);
+  }
+}
