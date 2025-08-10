1333524659ac66368d3cef4e225f1070c72d1a07
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-10 18:45:41 +0700

    More on create and save

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserting user with ID', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updating user with ID', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removing user with ID', this.id);
+  }
 }
