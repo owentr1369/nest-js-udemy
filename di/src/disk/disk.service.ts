0c531deed68fa786d659338deb39a499b379cb99
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-10 16:15:30 +0700

    more on di between modules

--- di/src/disk/disk.service.ts
+++ di/src/disk/disk.service.ts
@@ -1,4 +1,12 @@
+import { PowerService } from 'src/power/power.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+  getData() {
+    console.log('Drawing 20 watts of power');
+    this.powerService.supplyPower(20);
+    return 'data!';
+  }
+}
