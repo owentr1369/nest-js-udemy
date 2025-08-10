cd8d66a84bb6a6eda18356cef4770229a01fa718
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-10 18:53:00 +0700

    removing users

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -13,7 +13,7 @@ export class UsersService {
     return this.repo.save(user);
   }
   findOne(id: number) {
-    return this.repo.findOneBy({ id });
+    return this.repo.findOne({ where: { id } });
   }
   find(email: string) {
     return this.repo.find({ where: { email } });
@@ -30,7 +30,11 @@ export class UsersService {
     Object.assign(existingUser, attrs);
     return this.repo.save(existingUser);
   }
-  remove(id: number) {
-    return this.repo.delete(id);
+  async remove(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.repo.remove(user);
   }
 }
