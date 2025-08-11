dc6d1f589b1d0ea552fc70fa3b42cb01075d4e39
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-11 22:13:06 +0700

    Add test for signin with unused email in AuthService

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -43,4 +43,10 @@ describe('AuthService', () => {
       BadRequestException,
     );
   });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(BadRequestException);
+  });
 });
