242132233e7e7cd50532a7c4d16278dcb59ecc25
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-11 22:08:04 +0700

    ensuring password gets hashed

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -4,25 +4,35 @@ import { UsersService } from './users.service';
 import { User } from './user.entity';
 
 describe('AuthService', () => {
-  it('Can create an instance of auth service', async () => {
-    // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
     };
-    const fakeAuthService: Partial<AuthService> = {
-      signup: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
-    };
+
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
         { provide: UsersService, useValue: fakeUsersService },
-        { provide: AuthService, useValue: fakeAuthService },
       ],
     }).compile();
-    const service = module.get(AuthService);
+
+    service = module.get(AuthService);
+  });
+
+  it('Can create an instance of auth service', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Create a new user with a salted and hashed password', async () => {
+    const user = await service.signup('asdf@asdf.com', 'asdf');
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });
