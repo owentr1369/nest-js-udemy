49354102b035ecf78c6d36c602bb8c7842d97e70
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-11 20:29:30 +0700

    setting up session

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import CookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  app
+    .use(
+      CookieSession({
+        keys: ['abcxyz123'],
+      }),
+    )
+    .useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
