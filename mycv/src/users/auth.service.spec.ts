dbca95894be94aed042d15b12b54863bd84b229c
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-11 21:19:05 +0700

    getting typescript to help with mocks

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -5,15 +5,20 @@ import { User } from './user.entity';
 
 it('Can create an instance of auth service', async () => {
   // Create a fake copy of the users service
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User),
+  };
+  const fakeAuthService: Partial<AuthService> = {
+    signup: (email: string, password: string) =>
       Promise.resolve({ id: 1, email, password } as User),
   };
   const module = await Test.createTestingModule({
     providers: [
       AuthService,
       { provide: UsersService, useValue: fakeUsersService },
+      { provide: AuthService, useValue: fakeAuthService },
     ],
   }).compile();
   const service = module.get(AuthService);
