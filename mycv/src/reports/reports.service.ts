41e6d239f94e86795f481fbf3442cdecfa21cc4c
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-12 17:48:26 +0700

    saving a report with the reports service

--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-reports.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './reports.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
   create(reportDto: CreateReportDto) {
-    return 'report created';
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
   }
 }
