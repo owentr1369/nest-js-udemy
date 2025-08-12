64885675bbbeb4d187d105804f169927075f9361
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-12 17:37:21 +0700

    add dto for report creation

---
+++ mycv/src/reports/dtos/create-reports.dto.ts
@@ -0,0 +1,9 @@
+export class CreateReportDto {
+  make: string;
+  model: string;
+  year: number;
+  mileage: number;
+  lng: number;
+  lat: number;
+  price: number;
+}
--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-reports.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  @Post()
+  createReport(@Body() body: CreateReportDto) {
+    return 'report created';
+  }
+}
