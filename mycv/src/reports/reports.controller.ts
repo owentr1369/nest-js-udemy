753c107e7a2713f95883ef8dface1c39db3041c4
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-12 20:28:59 +0700

    transforming properties with a DTO

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -4,14 +4,21 @@ import { CreateReportDto } from './dtos/create-reports.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
-    console.log(body);
+    if (!user) {
+      throw new Error(
+        'User is null or undefined - check CurrentUser decorator implementation',
+      );
+    }
     return this.reportsService.create(body, user);
   }
 }
