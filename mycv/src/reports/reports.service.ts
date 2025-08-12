01a69a169c225a9b1641d6bfd487044c32e4aeee
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-12 17:46:57 +0700

    receiving report creation requests

--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-reports.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  create(reportDto: CreateReportDto) {
+    return 'report created';
+  }
+}
