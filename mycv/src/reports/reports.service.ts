41ab2e2553c7c86ff6a8b063661d7058a02094ec
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-13 19:40:26 +0700

    creating a querying builder

--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -22,9 +22,15 @@ export class ReportsService {
     return this.repo.update(id, { approved });
   }
   createEstimate(estimateDto: GetEstimateDto) {
-    return this.repo.find({
-      select: ['price'],
-      where: estimateDto,
-    });
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make: estimateDto.make })
+      .andWhere('model = :model', { model: estimateDto.model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: estimateDto.lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: estimateDto.lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year: estimateDto.year })
+      .andWhere('approved IS TRUE')
+      .getRawOne();
   }
 }
