12928ddd0ee16844d144a54a8a9b6646b1230710
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-13 19:49:53 +0700

    providing the cookie key

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/reports.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import cookieSession from 'cookie-session';
 
 @Module({
   imports: [
@@ -37,4 +38,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get('COOKIE_KEY') || ''],
+        }),
+      )
+      .forRoutes('8');
+  }
+}
